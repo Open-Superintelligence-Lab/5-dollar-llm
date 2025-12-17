4f5e9dea124789cf874f664889fc629466ed87d3
Rohan Khan <rk8399165+github@gmail.com>
2025-12-17 09:11:22 +0600

    fix conflict with train and debug moe.

--- debug_llm.py
+++ debug_llm.py
@@ -13,7 +13,7 @@ from configs.dataset_config import DataConfig
 from training.trainer import train_moe_model
 from utils.helpers import set_seed
 from utils.logger import setup_logging
-from train_moe import prepare_datasets
+from train_llm import prepare_datasets
 
 def main():
     logger = setup_logging(log_dir="./logs_debug")
